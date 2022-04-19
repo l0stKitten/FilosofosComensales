9d61c9b817182905db2500d37de10fc3965eb90a
d.lopez <dlopezf@ulasalle.edu.pe>
2022-04-18 22:49:41 -0500

    Prueba secuencial de filosofos

--- filosofos.cpp
+++ filosofos.cpp
@@ -1,6 +1,20 @@
 #include<stdio.h>
 
+// Metodo filosofo comer
+void filosofo (char *arg);
+
 int main(void){
-	printf("Hola Mundo");
+	char filo[20] = "Epicuro";
+	char filo2[20] = "Confucio";
+
+	filosofo(filo);
+	filosofo(filo2);
+	
 	return 0;
+}
+
+void filosofo (char *arg){
+	for (int i=1; i<=5; i++){
+		printf("%s estoy comiendo : %d \n", arg, i);
+	}
 }
