1018206026b5b5c9a3b946c0d313d58bfd7ad2b1
d.lopez <dlopezf@ulasalle.edu.pe>
2022-04-20 01:15:49 -0500

    Implementación del nombre de los Filósofos

--- filosofos.cpp
+++ filosofos.cpp
@@ -1,8 +1,10 @@
 #include<stdio.h>
 #include<pthread.h>
+#include<string.h>
+#include<stdlib.h>
 
 //Definimos globalmente un número de filosofos
-#define NUM_FILOSOFOS 10
+#define NUM_FILOSOFOS 5
 
 // Metodo filosofo comer
 void *comer (void *arg);
@@ -11,19 +13,37 @@ void *comer (void *arg);
 int variable = 0;
 
 int main(void){
-	char filo[20] = "Epicuro";
-	char filo2[20] = "Confucio";
-
+	//Array de nombres de filósofos
+	char nomFilo[10][20] = {"Confucio", "Pitágoras", "Platón", "Sócrates", "Epicurio", "Tales", "Heráclito", "Diógenes", "Sófocles", "Zenón"};
+	
+	//Estado de los nombres
+	int estNom[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	
 	printf("Variable: %d \n", variable);
 	
 	pthread_t filosofos[NUM_FILOSOFOS];
-	int identificadores[NUM_FILOSOFOS];
+	//int identificadores[NUM_FILOSOFOS];
 
 	int i;
+	int j;
 	//Creacion de los filosofos
 	for (i = 0; i < NUM_FILOSOFOS; i++){
-		identificadores[i] = i+1;
-		pthread_create(&filosofos[i], NULL, &comer, &identificadores[i]);
+		//Si el nombre no ha sido usado, NUM_FILOSOFOS < 10
+		if (i%10 == 0){
+			j = 0;
+		}
+		if (estNom[j] == 0){
+			pthread_create(&filosofos[i], NULL, &comer, &nomFilo[j]);
+			estNom[j] += 1;
+		//Si el nombre ha sido usado, NUM_FILOSOFOS > 10
+		} else{
+			char num[1];
+			snprintf(num, sizeof(num), "%d", estNom[j]);
+			strcat(nomFilo[j], num);
+			pthread_create(&filosofos[i], NULL, &comer, &nomFilo[j]);
+			estNom[i] += 1;
+		}
+		j++;
 	}
 
 	//Create Inicial
@@ -37,6 +57,7 @@ int main(void){
 	
 	//Joint del arreglo de filosofos
 	for (i = 0; i < NUM_FILOSOFOS; i++){
+
 		pthread_join(filosofos[i], NULL);	
 	}
 	//pthread_join(epicurio, NULL);
@@ -50,11 +71,11 @@ int main(void){
 //Se hace uso del recurso compartido
 void *comer (void *arg){
 
-	//char *nombre = (char *)arg;
-	int ident = *((int *)arg);
-	for (int i=1; i<=100; i++){
+	char *nombre = (char *)arg;
+	//int ident = *((int *)arg);
+	for (int i=1; i<=3; i++){
 		variable = variable + 1;
-		//printf("Filósofo - %d está comiendo : %d . Variable = %d \n", ident, i, variable);
+		printf("Filósofo - %s está comiendo : %d . Variable = %d \n", nombre, i, variable);
 	}
 	return NULL;
 }
